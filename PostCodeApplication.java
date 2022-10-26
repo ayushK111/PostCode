a9ff059b114cec5e5c6ae31747d6574fbba29946
ayushK111 <38379079+ayushK111@users.noreply.github.com>
2022-10-26 10:52:48 +0530

    Add files via upload

---
+++ PostCodeApplication.java
@@ -0,0 +1,15 @@
+package com.sample.restapi.postcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostCodeApplication.class, args);
+	}
+
+	
+	
+	  }
